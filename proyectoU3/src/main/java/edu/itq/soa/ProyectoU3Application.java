01fb78ad4c3df493d1ee81edc25c4ce00b9c8da5
Cinthya97Almaguer <99464245+Cinthya97Almaguer@users.noreply.github.com>
2024-10-21 21:54:08 -0600

    Creacion de Proyecto

---
+++ proyectoU3/src/main/java/edu/itq/soa/ProyectoU3Application.java
@@ -0,0 +1,13 @@
+package edu.itq.soa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU3Application.class, args);
+	}
+
+}
